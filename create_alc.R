46735da9393a34055e068d37ba25be200dea1af8
Agatha Priandini <agathapriandini@gmail.com>
2023-11-21 13:46:46 +0700

    Data wrangling for week 3

---
+++ create_alc.R
@@ -0,0 +1,5 @@
+#Agatha Priandini
+#21 Nov 2023
+
+##This script is used for the data wrangling section for week 3's assignment, the data is taken from http://www.archive.ics.uci.edu/dataset/320/student+performance
+
